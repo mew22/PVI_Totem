e2eca69f4fe9486c93f8338714503f0e65502030
Sebastien Delaherche <sebastien.delaherche@gmail.com>
2024-01-17 13:52:18 +0100

    Create Accueil Porte ouverte totem

---
+++ accueil/index.js
@@ -0,0 +1,14 @@
+pageList = [
+    "page/accueil.html",
+    "page/innovant.html",
+    "page/actu.html",
+    "page/porte_ouverte.html",
+    "page/projet_pro_2024.html",
+    "page/cybersecu.html",
+    "page/metiers.html",
+]
+
+pageAccueil = [
+    "page/accueil.html",
+    "page/porte_ouverte.html",
+]
