c31d96524a8700f5c06e4f3a8fb437c6143a8281
LauraneDesbois <desboislaurane@gmail.com>
2024-02-01 12:11:40 +0100

    Update pvi totem note de service

--- pvi/index.js
+++ pvi/index.js
@@ -4,11 +4,18 @@ pageList = [
     "../page/metiers.html",
     "../page/epi.html",
     "../page/covid.html",
+    "../page/frais_prestaires.html",
+    "../page/changement_janvier_1.html",
+    "../page/PVI_changement_janvier_2.html",
+    "../page/changement_janvier_3.html",
     "page/bonne_journee.html",
     "../page/traffic_etech.html",
     "../page/actu.html",
     "../page/projet_pro_2024.html",
     "../page/cybersecu.html",
+    "../page/conge_exceptionnels_1.html",
+    "../page/conge_exceptionnels_2.html",
+    "../page/conge_exceptionnels_3.html",
 ]
 
 pageAccueil = [
